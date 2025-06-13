cf7e9ab3fc4711d531cf12fb9b787b1acfb3e172
butchhartman <rcamacho5436@gmail.com>
2025-06-13 15:46:59 -0700

    Implemented camera controls. The user is now free to fly around and observe the triangle.

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,32 @@
 #include <gml.hpp>
 #include <GameWindow.h>
 #include <glad/glad.h>
+#define RAD(angleMeasure) angleMeasure * (3.14159265358979323846 / 180.0)
+
 
 // Icky, add as a gwl function to retrieve these values
 float windowWidth = 800.0f;
 float windowHeight = 600.0f;
 
-void inputCallback(GameWindow* window, gwInputEvent event) {
-    switch (event.eventType) {
-        case gw_windowReizeEvent:
-            glViewport(0, 0, event.windowWidth, event.windowHeight);
-            windowWidth = (float)event.windowWidth;
-            windowHeight = (float)event.windowHeight;
-            break;
-        default:
-            break;
-    }
-}
+const float cameraSpeed = 2.5f;
+const float sensitivity = 0.05f;
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+float xOffset = 0.0f;
+float yOffset = 0.0f;
+float lastX = 0.0f;
+float lastY = 0.0f;
+
+gml::Vec3 cameraPos = gml::Vec3(0.0f, 0.0f, 3.0f);
+gml::Vec3 cameraFront = gml::Vec3(0.0f, 0.0f, -1.0f);
+gml::Vec3 cameraUp = gml::Vec3(0.0f, 1.0f, 0.0f);
+
+bool forward = false;
+bool backward = false;
+bool right = false;
+bool left = false;
 
 float vertices[] = {
     -0.5f, -0.5f, 0.0f,
@@ -26,10 +36,81 @@ float vertices[] = {
     0.0f, 0.5f, 0.0f
 };
 
+void inputCallback(GameWindow* window, gwInputEvent event) {
+    switch (event.eventType) {
+        case gw_windowReizeEvent:
+            glViewport(0, 0, event.windowWidth, event.windowHeight);
+            windowWidth = (float)event.windowWidth;
+            windowHeight = (float)event.windowHeight;
+            lastX = windowWidth/2;
+            lastY = windowHeight/2;
+            break;
+
+        case gw_keyboardEvent:
+            if (event.key == gw_W && event.keyStateFlags & KEY_DOWN_BIT)  {
+                forward = true;
+            } else if (event.key == gw_W && event.keyStateFlags & KEY_UP_BIT) {
+                forward = false;
+            }
+            if (event.key == gw_A && event.keyStateFlags & KEY_DOWN_BIT)  {
+                left = true;
+            } else if (event.key == gw_A && event.keyStateFlags & KEY_UP_BIT) {
+                left = false;
+            }
+            if (event.key == gw_S && event.keyStateFlags & KEY_DOWN_BIT)  {
+                backward = true;
+            } else if (event.key == gw_S && event.keyStateFlags & KEY_UP_BIT) {
+                backward = false;
+            }
+            if (event.key == gw_D && event.keyStateFlags & KEY_DOWN_BIT)  {
+                right = true;
+            } else if (event.key == gw_D && event.keyStateFlags & KEY_UP_BIT) {
+                right = false;
+            }
+            break;
+
+        case gw_mouseEvent:
+            xOffset = event.xPos - lastX; 
+            yOffset = lastY - event.yPos;
+           
+            // lastX = (float)event.xPos;
+            // lastY = (float)event.yPos;
+
+            xOffset *= sensitivity;
+            yOffset *= sensitivity;
+            
+            // Must constrain yaw vaules to or else floating point precision
+            // will be lost with high yaw values. Thanks to Lonami's comment on
+            // https://learnopengl.com/Getting-started/Camera
+            yaw = std::fmod(yaw + xOffset, 360.0f);
+            pitch += yOffset;
+            break;
+
+        default:
+            break;
+    }
+}
+
+void updateCameraPos(double deltaTime) {
+    if (forward) {
+        cameraPos = cameraPos + cameraFront * cameraSpeed * deltaTime;
+    }
+    if (left) {
+        cameraPos = cameraPos - (cameraFront.cross(cameraUp)).normalize() * cameraSpeed * deltaTime;
+    }
+    if (backward) {
+        cameraPos = cameraPos - cameraFront * cameraSpeed * deltaTime;
+    }
+    if (right) {
+        cameraPos = cameraPos + (cameraFront.cross(cameraUp)).normalize() * cameraSpeed * deltaTime;
+    }
+}
+
 int main() {
     gwlPrintVersion();
 
     GameWindow* window = gwlCreateWindow("Voxel Survival Game :v");
+    gwlSetInputFlags(window, CAPTURE_MOUSE_BIT, 1);
     gwlCreateOpenGLContext(&window);
 
     // There is no loader function defined for gwl, so use this instead
@@ -38,6 +119,7 @@ int main() {
     } 
 
     gwlSetInputCallback(window, inputCallback);
+    gwlPrintGLVersion(window);
     gwlShowWindow(window);
 
     glClearColor(0.0f, 0.0f, 0.05f, 1.0f);
@@ -57,38 +139,52 @@ int main() {
     glEnableVertexAttribArray(0);
 
     gml::Mat4 myTranslationMat = gml::Mat4::translation(0.0f, 0.0f, 0.0f);
+
     gml::Mat4 myViewMat = gml::Mat4::lookAt(
-        gml::Vec3(4.0f, 0.0f, 3.0f),
-        gml::Vec3(0.0f, 0.0f, 0.0f),
-        gml::Vec3(0.0f, 1.0f, 0.0f)
+        cameraPos,
+        cameraPos + cameraFront,
+        cameraUp
     );
-    // gml::Mat4 myViewMat = gml::Mat4();
+
     gml::Mat4 myPerspectiveMat = 
         gml::Mat4::perspectiveProjection(.78539816f, windowWidth/windowHeight, 0.1f, 400.0f);
 
+    GLuint modelMatLocation = 
+        glGetUniformLocation(BasicShader.getID(), "modelMat");
 
-    gml::Vec4  NDCvec 
-        = myPerspectiveMat * myViewMat * myTranslationMat * gml::Vec4(-0.5f, -0.5f, 0.0f, 1.0f);
+    GLuint viewMatLocation = 
+        glGetUniformLocation(BasicShader.getID(), "viewMat");
+
+    GLuint perspectiveMatLocation = 
+        glGetUniformLocation(BasicShader.getID(), "perspectiveMat");
+
+    double lastTime = gwlGetTime(window);
+    double elapsedTime;
 
     while (gwlGetWindowStatus(window) == GW_WINDOW_ACTIVE) {
+        cameraFront = gml::Vec3(
+            std::cosf(RAD(yaw)) * std::cosf(RAD(pitch)),
+            std::sinf(RAD(pitch)),
+            std::sinf(RAD(yaw)) * std::cosf(RAD(pitch))
+        ).normalize();
 
         glClear(GL_COLOR_BUFFER_BIT);
 
         BasicShader.use();
 
-        GLuint modelMatLocation = 
-            glGetUniformLocation(BasicShader.getID(), "modelMat");
         glUniformMatrix4fv(modelMatLocation, 1, GL_FALSE, myTranslationMat.getData());
 
-        GLuint viewMatLocation = 
-            glGetUniformLocation(BasicShader.getID(), "viewMat");
+        gml::Mat4 myViewMat = gml::Mat4::lookAt(
+            cameraPos,
+            cameraPos + cameraFront,
+            cameraUp
+        );
+
         glUniformMatrix4fv(viewMatLocation, 1, GL_FALSE, myViewMat.getData());
 
         myPerspectiveMat =  
             gml::Mat4::perspectiveProjection(0.78539816f, windowWidth/windowHeight, 0.1f, 400.0f);
         
-        GLuint perspectiveMatLocation = 
-            glGetUniformLocation(BasicShader.getID(), "perspectiveMat");
         glUniformMatrix4fv(perspectiveMatLocation, 1, GL_FALSE, myPerspectiveMat.getData());
 
         glBindVertexArray(VAO);
@@ -96,6 +192,11 @@ int main() {
 
         gwlSwapBuffers(window);
         gwlPollEvents(window);
+
+        elapsedTime = gwlGetTime(window) - lastTime;
+        lastTime = gwlGetTime(window);
+
+        updateCameraPos(elapsedTime);
     }
 
     gwlCleanupWindow(window);
